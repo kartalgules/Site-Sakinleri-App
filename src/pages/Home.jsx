bcea15b7f6a7e586130cdfa2b45186b6ed2aa785
Kartal Gules <kartalgules@gmail.com>
2023-11-26 05:43:45 +0300

    Main page editing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import style from "../styles/Home.module.css"
 
 function Home() {
-  return <div>Home</div>;
+  return(
+    <div className={style.main_container}>
+      <img src="../../public/Main.png" alt="" />
+    </div>
+  )
 }
 
 export default Home;
