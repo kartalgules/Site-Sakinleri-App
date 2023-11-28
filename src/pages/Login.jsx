c7270a00949066c042ced1a81f8fbb1290effe59
Kartal Gules <kartalgules@gmail.com>
2023-11-29 00:11:57 +0300

    Login page started

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import style from "../styles/Login.module.css";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={style.login_container}>
+      <h1>Giriş Yapın</h1>
+      <form className={style.login} htmlFor="">
+        <input type="text" placeholder={"Kullanıcı adınızı giriniz"} required />
+        <input type="password" placeholder={"Şifrenizi giriniz"} required />
+        <button>Login</button>
+        <label name="remember">
+          Beni Haırla
+          <input type="checkbox" name="remember" />
+        </label>
+        <a href="#">Şifremi unuttum?</a>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
