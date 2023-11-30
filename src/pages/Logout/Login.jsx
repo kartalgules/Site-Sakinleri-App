b4f832db12959c897bedd603d6093de741a1202c
Kartal Gules <kartalgules@gmail.com>
2023-11-30 15:57:36 +0300

    Login edit

--- src/pages/Logout/Login.jsx
+++ src/pages/Logout/Login.jsx
@@ -1,34 +1,60 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import style from "../../styles/Login.module.css";
 import { Form, Input, Button } from "antd";
-import userData from '../../../data/users.json'
+import userData from "../../../data/users.json";
 
 function Login() {
-  let isLogin = false
+  let isLogin = false;
+  const [loginCheck, setLoginCheck] = useState("");
+  const cheking = () => {
+    setLoginCheck(
+      <p>{isLogin ? "Giriş Başarılı" : "Giriş Başarısız.Tekrar Deneyin"}</p>
+    );
+  };
+
   const onFinish = (values) => {
-    localStorage.setItem("username", JSON.stringify(values.username))
-    localStorage.setItem("password", JSON.stringify(values.password))
-    values.username == userData[0].username && values.password == userData[0].password  ? isLogin = true : null;
-  }
+    localStorage.setItem("username", JSON.stringify(values.username));
+    localStorage.setItem("password", JSON.stringify(values.password));
+    values.username == userData[0].username &&
+    values.password == userData[0].password
+      ? (isLogin = true)
+      : null;
+    cheking();
+  };
   return (
     <div className={style.login_container}>
-      <h1>Giriş Yapın</h1>
-      <Form layout="vertical" onFinish={onFinish}>
-        <Form.Item name={"username"}>
-          <Input placeholder="Kullanıcı adınızı giriniz" required />
-        </Form.Item>
-        <Form.Item name={"password"}>
-          <Input placeholder="Şifrenizi giriniz" required />
-        </Form.Item>
-        <Button htmlType="submit" type="primary">Giriş Yap</Button>
-      </Form>
-
-      {/* <label name="remember">
-        Beni Haırla
-        <input type="checkbox" name="remember" />
-      </label>
-      <a href="#">Şifremi unuttum?</a> */}
+      <div className={style.login_form}>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item name={"username"}>
+            <Input
+              style={{ width: "85%", height: "3rem", fontWeight: "700" }}
+              placeholder="Kullanıcı adınızı giriniz"
+              required
+            />
+          </Form.Item>
+          <Form.Item name={"password"}>
+            <Input
+              style={{ width: "85%", height: "3rem", fontWeight: "700" }}
+              placeholder="Şifrenizi giriniz"
+              required
+            />
+          </Form.Item>
+          <Button
+            style={{
+              width: "40%",
+              height: "20%",
+              fontWeight: "600",
+              background: "#8c8c8c",
+            }}
+            htmlType="submit"
+            type="primary"
+          >
+            Giriş Yap
+          </Button>
+          {loginCheck}
+        </Form>
+      </div>
     </div>
   );
 }
