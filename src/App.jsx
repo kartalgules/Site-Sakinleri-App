f76593b9c6738902683afc475de6f5ff3588344d
Kartal Gules <kartalgules@gmail.com>
2023-12-03 23:56:40 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 /* eslint-disable no-unused-vars */
-import { useContext } from "react";
-import LoginContext from "./context/LoginContext";
+
 import Login from "./pages/Login";
-import Logout from "./pages/Logout";
 
 function App() {
-  const data = useContext(LoginContext);
-
-  return <>{data.login ? <Login /> : <Logout />}</>;
+  return <Login /> 
 }
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,47 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Login/Home";
+import LoginContext from "../context/LoginContext";
+import Navbar from "../components/Navbar";
+import HomeLogin from "./Login/Home";
 import List from "./Login/List";
 import Add from "./Login/Add";
 import ExcelAdd from "./Login/ExcelAdd";
 import BulkDelete from "./Login/BulkDelete";
 import Edit from "./Login/Edit";
 import Settings from "./Login/Settings";
-import Logout from "./Login/Logout";
-import Navbar from "../components/Navbar";
+import LogoutButton from "./Login/Logout";
 import BadRequest from "./BadRequest";
+import HomeLogout from "./Logout/Home";
+import GoLogin from "./Logout/Login";
+import Singin from "./Logout/Singin";
 
 function Login() {
+  const data = useContext(LoginContext);
   return (
+    <>
+      {data.login ? (
         <Navbar>
+          <Routes>
+            <Route path="/" exact="true" element={<HomeLogin />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="/excelAdd" element={<ExcelAdd />} />
+            <Route path="/bulkDelete" element={<BulkDelete />} />
+            <Route path="/edit" element={<Edit />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/logout" element={<LogoutButton />} />
+            <Route path="/*" element={<BadRequest />} />
+          </Routes>
+        </Navbar>
+      ) : (
         <Routes>
-          <Route path="/" exact="true" element={<Home />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/excelAdd" element={<ExcelAdd />} />
-          <Route path="/bulkDelete" element={<BulkDelete />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/logout" element={<Logout />} />
+          <Route path="/" element={<HomeLogout />} />
+          <Route path="/singin" element={<Singin />} />
+          <Route path="/login" element={<GoLogin />} />
           <Route path="/*" element={<BadRequest />} />
         </Routes>
-      </Navbar>
+      )}
+    </>
   );
 }
 
