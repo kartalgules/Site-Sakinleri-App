0b8fb73590f9d3c8b1ef75b593d3b1c2385dc9f1
Kartal Gules <kartalgules@gmail.com>
2023-12-06 23:52:53 +0300

    404 page edit

--- src/pages/BadRequest.jsx
+++ src/pages/BadRequest.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
-
+import style from "../styles/BadRequest.module.css";
 function BadRequest() {
   return (
-    <div>
-      <h2>404 Sayfa Bulunamadı</h2>
-      <NavLink to={'/'}>Anasayfa&apos;ya dön</NavLink>
+    <div className={style.main}>
+      <h1>404 Sayfa Bulunamadı</h1>
+      <br />
+      <NavLink to={"/"}>Anasayfa&apos;ya dön</NavLink>
     </div>
   );
 }
