5441e84390e50f9b5a09cfb42fef9682744bb327
Kartal Gules <kartalgules@gmail.com>
2023-12-13 00:23:00 +0300

    Sing İn error message edit

--- src/pages/Logout/Singin.jsx
+++ src/pages/Logout/Singin.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import {useFormik} from "formik";
+import { useFormik } from "formik";
 import { basicSchema } from "../../schemas";
-import style from '../../styles/SingIn.module.css'
+import style from "../../styles/SingIn.module.css";
 // import { useNavigate } from "react-router-dom";
 // import { singUp } from "../../firebase";
 
@@ -11,14 +11,13 @@ function Singin() {
       fullName: "",
       email: "",
       password: "",
-      confirmPassword:""
+      confirmPassword: "",
     },
     onSubmit: (values) => {
       console.log(values.email);
     },
-    validationSchema:basicSchema,
+    validationSchema: basicSchema,
   });
-  
   // const [fullName, setFullName] = useState("");
   // const [mail, setMail] = useState("");
   // const [password, setPassword] = useState("");
@@ -47,43 +46,53 @@ function Singin() {
   return (
     <div className={style.form_container}>
       <form onSubmit={handleSubmit}>
-      <label>Adınız</label>
-        <input
-          type="fullName"
-          id="fullName"
-          placeholder="Adınızı giriniz.."
-          value={values.fullName}
-          onChange={handleChange}
-        />
-        {errors.fullName && <p className="error">{errors.fullName}</p>}
-        <label>Email</label>
-        <input
-          type="email"
-          id="email"
-          placeholder="Mail adresinizi giriniz.."
-          value={values.email}
-          onChange={handleChange}
-          className={errors.email ? 'input-error' : ''}
-        />
-        {errors.email && <p className="error">{errors.email}</p>}
-        <label>Şifre</label>
-        <input
-          type="password"
-          id="password"
-          placeholder="Şifrenizi giriniz.."
-          value={values.password}
-          onChange={handleChange}
-        />
-        {errors.password && <p className="error">{errors.password}</p>}
-        <label>Şifre Onayı</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          placeholder="Şifrenizi tekrar giriniz.."
-          value={values.confirmPassword}
-          onChange={handleChange}
-        />
-        {errors.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+        <div className={style.sections}>
+          <label>Adınız</label>
+          <input
+            type="fullName"
+            id="fullName"
+            placeholder="Adınızı giriniz.."
+            value={values.fullName}
+            onChange={handleChange}
+          />
+          {errors.fullName && <p className={style.error}>{errors.fullName}</p>}
+        </div>
+        <div className={style.sections}>
+          <label>Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Mail adresinizi giriniz.."
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p className={style.error}>{errors.email}</p>}
+        </div>
+        <div className={style.sections}>
+          <label>Şifre</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Şifrenizi giriniz.."
+            value={values.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p className={style.error}>{errors.password}</p>}
+        </div>
+        <div className={style.sections}>
+          <label>Şifre Onayı</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Şifrenizi tekrar giriniz.."
+            value={values.confirmPassword}
+            onChange={handleChange}
+          />
+          {errors.confirmPassword && (
+            <p className={style.error}>{errors.confirmPassword}</p>
+          )}
+        </div>
+
         <button type="submit">Gönder</button>
       </form>
     </div>
