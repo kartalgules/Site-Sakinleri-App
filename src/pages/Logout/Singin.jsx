e8a357ba2a8c88b23859317ad39b3856a170fb05
Kartal Gules <kartalgules@gmail.com>
2023-12-12 22:42:31 +0300

    Validation procedures are being carried out

--- src/pages/Logout/Singin.jsx
+++ src/pages/Logout/Singin.jsx
@@ -1,61 +1,85 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { singUp } from "../../firebase";
+import {useFormik} from "formik";
+import { basicSchema } from "../../schemas";
 import style from '../../styles/SingIn.module.css'
+// import { useNavigate } from "react-router-dom";
+// import { singUp } from "../../firebase";
 
 function Singin() {
+  const { handleChange, handleSubmit, values } = useFormik({
+    initialValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      confirmPassword:""
+    },
+    validationSchema:basicSchema,
+    onSubmit: (values) => {
+      console.log(values.email);
+    },
+  });
   
-  const [fullName, setFullName] = useState("");
-  const [mail, setMail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+  // const [fullName, setFullName] = useState("");
+  // const [mail, setMail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const navigate = useNavigate();
 
-  const handleFullName = (e) => {
-    setFullName(e.target.value);
-  };
-  const handleMail = (e) => {
-    setMail(e.target.value);
-  };
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    singUp(fullName, mail, password)
-      .then(() => {
-        navigate("/login");
-        console.log("Done!..")
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+  // const handleFullName = (e) => {
+  //   setFullName(e.target.value);
+  // };
+  // const handleMail = (e) => {
+  //   setMail(e.target.value);
+  // };
+  // const handlePassword = (e) => {
+  //   setPassword(e.target.value);
+  // };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   singUp(fullName, mail, password)
+  //     .then(() => {
+  //       navigate("/login");
+  //       console.log("Done!..")
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //     });
+  // };
   return (
     <div className={style.form_container}>
-      <form >
-        <label>İsminiz</label>
+      <form onSubmit={handleSubmit}>
+      <label>Adınız</label>
         <input
-          type="text"
-          placeholder="İsminizi giriniz.."
-          value={fullName}
-          onChange={handleFullName}
+          type="fullName"
+          id="fullName"
+          placeholder="Adınızı giriniz.."
+          value={values.fullName}
+          onChange={handleChange}
         />
-        <label>Mail Adresiniz</label>
+        <label>Email</label>
         <input
           type="email"
+          id="email"
           placeholder="Mail adresinizi giriniz.."
-          value={mail}
-          onChange={handleMail}
+          value={values.email}
+          onChange={handleChange}
         />
-        <label>Parolanız</label>
+        <label>Şifre</label>
         <input
           type="password"
-          placeholder="Parolanızı giriniz.."
-          value={password}
-          onChange={handlePassword}
+          id="password"
+          placeholder="Şifrenizi giriniz.."
+          value={values.password}
+          onChange={handleChange}
         />
-        <button onClick={handleSubmit}>Kayıt ol</button>
+        <label>Şifre Onayı</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          placeholder="Şifrenizi tekrar giriniz.."
+          value={values.confirmPassword}
+          onChange={handleChange}
+        />
+        <button type="submit">Gönder</button>
       </form>
     </div>
   );
---
+++ src/schemas/index.js
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+
+export const basicSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Geçerli bir email giriniz")
+    .required("Email girmek zorunludur"),
+  fullName: yup.string().name().required("Lütfen adınızı giriniz"),
+  password: yup
+    .string()
+    .min(6, "Lütfen minimum 6 karakter giriniz")
+    .matches(passwordRules, {
+      message: "Lütfen en az 1 büyük harf,1 küçük harf ve 1 sayı giriniz.",
+    })
+    .required("Şifre girmek zorunludur"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Şifreler eşleşmiyor")
+    .required("Şifre girmek zorunludur"),
+});
