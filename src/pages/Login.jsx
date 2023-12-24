0e8ea0cbc98ef248b04b507394388231f27e4988
Kartal Gules <kartalgules@gmail.com>
2023-12-24 23:50:00 +0300

    Context auth connected to login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { NavLink , Route, Routes } from "react-router-dom";
+import { NavLink, Route, Router, Routes } from "react-router-dom";
 import { singIn } from "../firebase";
 import Navbar from "../components/Navbar";
 import HomeLogin from "./Login/Home";
@@ -14,23 +14,29 @@ import BadRequest from "./BadRequest";
 import HomeLogout from "./Logout/Home";
 import GoLogin from "./Logout/Login";
 import Singin from "./Logout/Singin";
+import FirebaseContext from "../context/FirebaseContext";
+import { useContext } from "react";
 
 function Login() {
+  const { auth } = useContext(FirebaseContext);
+
   const İsLogin = () => {
     return (
       <>
         <Navbar>
-          <Routes>
-            <Route path="/" exact="true" element={<HomeLogin />} />
-            <Route path="/list" element={<List />} />
-            <Route path="/add" element={<Add />} />
-            <Route path="/excelAdd" element={<ExcelAdd />} />
-            <Route path="/bulkDelete" element={<BulkDelete />} />
-            <Route path="/edit" element={<Edit />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/logout" element={<LogoutButton />} />
-            <Route path="/*" element={<BadRequest />} />
-          </Routes>
+          <Router>
+            <Routes>
+              <Route path="/" exact="true" element={<HomeLogin />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/add" element={<Add />} />
+              <Route path="/excelAdd" element={<ExcelAdd />} />
+              <Route path="/bulkDelete" element={<BulkDelete />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/logout" element={<LogoutButton />} />
+              <Route path="/*" element={<BadRequest />} />
+            </Routes>
+          </Router>
         </Navbar>
       </>
     );
@@ -39,17 +45,19 @@ function Login() {
   const İsLogout = () => {
     return (
       <>
-        <Routes>
-          <Route path="/" element={<HomeLogout />} />
-          <Route path="/singin" element={<Singin />} />
-          <Route path="/login" element={<GoLogin />} />
-          <Route path="/*" element={<BadRequest />} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomeLogout />} />
+            <Route path="/singin" element={<Singin />} />
+            <Route path="/login" element={<GoLogin />} />
+            <Route path="/*" element={<BadRequest />} />
+          </Routes>
+        </Router>
       </>
     );
   };
 
-  return <>{singIn ? <İsLogin /> : <İsLogout />}</>;
+  return <>{ auth ? <İsLogin /> : <İsLogout />}</>;
 }
 
 export default Login;
