283ec5dbcd4103412b35674a56e90e37efa8bb0b
Kartal Gules <kartalgules@gmail.com>
2024-01-24 03:23:50 +0300

    The validator package has been integrated. It was used in user operations

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Sema = mongoose.Schema;
 
@@ -16,6 +17,18 @@ const userSchema = new Sema({
 });
 
 userSchema.statics.singup = async function(email,password){
+
+    if(!email || !password){
+        throw Error('Required field. Please fill out.')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Please enter a valid email.')
+    }
+
+    if(!validator.isStrongPassword(password)){
+        throw Error('The password is not strong enough.')
+    }
+    
     const userControl = await this.findOne({email});
 
     if(userControl){
